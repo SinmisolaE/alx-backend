3615d4493f5c495a312dff6647cc09f187caea56
SinmisolaE <sinmisolaajao@gmail.com>
2024-07-21 20:03:59 +0000

    updated

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+
+""" index_range from the previous task
+    and the following class into your code"""
 import csv
 import math
 from typing import List
 
 
 index_range = __import__('0-simple_helper_function').index_range
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -28,7 +33,7 @@ class Server:
         """ two integer arguments page with default value 1
             and page_size with default value 10
             assert to verify that both arguments are integers greater than 0.
-            return the appropriate page of the dataset (i.e. the correct list of rows).
+            return the appropriate page of the dataset
         """
         assert type(page) is int and page > 0
         assert type(page_size) is int and page > 0
