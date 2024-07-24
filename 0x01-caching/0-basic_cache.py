3bb23aca447cb675c32c1961926d2da68bd8b3ee
SinmisolaE <sinmisolaajao@gmail.com>
2024-07-24 22:27:35 +0000

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,14 +7,15 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ 
+    """
        must use self.cache_data - dictionary from the parent class BaseCaching
        caching system doesn’t have limit
     """
 
     def put(self, key, item):
-        """ 
-          assign to the dictionary self.cache_data the item value for the key key
+        """
+          assign to the dictionary self.cache_data
+          the item value for the key key
           key or item is None, this method should not do anything.
         """
         if key is not None and item is not None:
@@ -23,7 +24,8 @@ class BasicCache(BaseCaching):
     def get(self, key):
         """
             return the value in self.cache_data linked to key
-            If key is None or if the key doesn’t exist in self.cache_data, return None
+            If key is None or if the key doesn’t exist
+            in self.cache_data, return None
         """
         if key is None or key not in self.cache_data:
             return None
