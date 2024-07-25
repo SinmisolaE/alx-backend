60e01b9c53ab166b4bbe8a321f0fa9566df20080
SinmisolaE <sinmisolaajao@gmail.com>
2024-07-25 21:46:05 +0000

    3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
 """
-  class FIFOCache that inherits from BaseCaching
+  class LRUCache that inherits from BaseCaching
   and is a caching system
 """
 
 BaseCaching = __import__('base_caching').BaseCaching
+from collections import OrderedDict
 
 
-class FIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """
         must use self.cache_data
         can overload def __init__(self):
     """
+
     def __init__(self):
         """ initializes inherited properties
         """
         super().__init__()
+        self.sorted_cache = OrderedDict()
 
     def put(self, key, item):
         """
@@ -24,17 +27,20 @@ class FIFOCache(BaseCaching):
            If key or item is None, this method should not do anything
            If the number of items in self.cache_data
            is higher that BaseCaching.MAX_ITEMS:
-           you must discard the first item put in cache
+           you must discard the least recently item put in cache
            you must print DISCARD: with the key discarded
            and following by a new line
         """
         if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key not in self.cache_data:
+                    least_use = list(self.sorted_cache.keys())[0]
+                    print("DISCARD: {}".format(least_use))
+                    del self.cache_data[least_use]
+                    del self.sorted_cache[least_use]
             self.cache_data[key] = item
-
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                first_key = list(self.cache_data.keys())[0]
-                print('DISCARD: {}'.format(first_key))
-                del self.cache_data[first_key]
+            self.sorted_cache[key] = item
+            self.sorted_cache.move_to_end(key)
 
     def get(self, key):
         """
@@ -45,4 +51,5 @@ class FIFOCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return None
 
+        self.sorted_cache.move_to_end(key)
         return self.cache_data[key]
